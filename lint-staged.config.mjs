96ba1a5c25fe55096bda4043ce632a91d1bdc69b
1058039938@qq.com <aniviah@163.com>
2022-12-11 02:58:04 +0800

    feat: 测试lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,34 @@
+// export default {
+//   "src/**/*.{js,jsx}": ['eslint --fix', 'prettier --write']
+// }
+
+import micromatch from 'micromatch'
+
+// export default (allStagedFiles) => {
+//   console.log('allStagedFiles---', allStagedFiles);
+//   // const shFiles = micromatch(allStagedFiles, ['**/src/**/*.sh'])
+//   // if (shFiles.length) {
+//   //   return `printf '%s\n' "Script files aren't allowed in src directory" >&2`
+//   // }
+//   // const codeFiles = micromatch(allStagedFiles, ['**/*.js', '**/*.ts'])
+//   // const docFiles = micromatch(allStagedFiles, ['**/*.md'])
+//   // return [`eslint ${codeFiles.join(' ')}`, `mdl ${docFiles.join(' ')}`]
+
+//   return ['eslint --fix', 'prettier --write']
+// }
+
+// lint-staged.config.js
+
 export default {
-  "src/**/*.{js,jsx}": ['eslint --fix', 'prettier --write']
+  "src/**/*.{js,jsx}": (files) => {
+    console.log('files----', files)
+    // from `files` filter those _NOT_ matching `*test.js`
+    // const match = micromatch.not(files, '*test.js')
+    // return files.map((filename) => `prettier --write '${filename}'`)
+    // return `prettier --write '${files.join(' ')}'`
+    return [`eslint --fix '${files.join(' ')}'`, `prettier --write '${files.join(' ')}'`]
+    // return files.map((filename) => `eslint --fix '${filename}'`)
+  },
 }
 
 /**
