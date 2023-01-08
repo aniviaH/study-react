b9cb83c4e71677bcacd0b2e29cf73eb92d510001
1058039938@qq.com <aniviah@163.com>
2023-01-08 12:08:14 +0800

    feat: useMemo和useCallback

--- oh-my-kanban/src/App.js
+++ oh-my-kanban/src/App.js
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import logo from './logo.svg'
 import './App.css'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useMemo } from 'react'
 import {css} from '@emotion/react'
 
 const COLUMN_BG_COLORS = {
@@ -11,6 +11,14 @@ const COLUMN_BG_COLORS = {
   done: '#C0E8EA'
 }
 const DATA_STORE_KEY = 'kanban-data-store'
+
+function fibonacci (n) {
+  if (n <= 2) {
+    return n
+  }
+
+  return fibonacci(n-1) + fibonacci(n-2)
+}
 
 function App() {
   const [showAdd, setShowAdd] = useState(false)
@@ -30,6 +38,31 @@ function App() {
     { title: '测试任务-1', status: '2022-12-16 14:40' },
   ])
   const [isLoading, setIsLoading] = useState(true)
+
+  // useMemo的使用场景，缓存执行成本较高的计算结果
+  // const memoized = useMemo(() => createByHeavyComputing(a, b), [a, b]);
+      //    --------           ----------------------------------  ------
+      //       ^                            ^                         ^
+      //       |                            |                         |
+      //   工厂函数返回值                   工厂函数                  依赖值数组
+  const [num, setNum] = useState('10')
+  const sum = useMemo(() => {
+    const n = parseInt(num, 10)
+    return fibonacci(n)
+  }, [num])
+
+  // useCallback的使用场景，获得记忆化的函数，使用返回的函数传给子组件，可减少子组件的更新
+  // const memoizedFunc = useCallback(() => {/*省略*/}, [a, b]);
+      //    ------------               ---------------   -----
+      //         ^                            ^            ^
+      //         |                            |            |
+      //   记忆化的回调函数                   回调函数      依赖值数组
+  
+  // const memoizedFunc = useMemo(() => () => {/*省略*/}, [a, b]);
+      //    ------------           ---------------------   -----
+      //       ^                      ^  ---------------      ^
+      //       |                      |         ^             |
+      // 工厂函数返回的回调函数        工厂函数   回调函数        依赖值数组
 
   // 获取卡片列表数据
   useEffect(() => {
@@ -262,7 +295,7 @@ function App() {
         font-size: calc(10px + 2vmin);
         color: white;
       `}>
-        <h1>我的看板 <button onClick={handleSaveAll}>保存所有卡片</button></h1>
+        <h1>我的看板 <button onClick={handleSaveAll}>保存所有卡片</button> <span>fibonacci({num}): {sum}</span></h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <KanbanBoard>
