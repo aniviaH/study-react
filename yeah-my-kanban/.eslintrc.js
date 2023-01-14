74d75442f62f72f1b928fe2be128295532a008a4
1058039938@qq.com <aniviah@163.com>
2023-01-14 20:54:18 +0800

    style: 使用eslint检查代码格式，修复代码格式问题

--- yeah-my-kanban/.eslintrc.js
+++ yeah-my-kanban/.eslintrc.js
@@ -20,5 +20,20 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['evt'] }],
+    'no-unused-expressions': ['error', { allowShortCircuit: true }],
+    'react/prop-types': ['error', { skipUndeclared: true }],
+    'no-console': ['off'],
+    'react/no-unknown-property': ['error', { ignore: ['css', 'test', 'expect'] }],
+    'linebreak-style': ['off'],
+    'react/jsx-no-bind': ['error', { allowBind: true, allowArrowFunctions: true }],
+    'max-len': ['off'],
+    'no-unused-vars': ['off'],
+    'no-use-before-define': ['error', {
+      functions: false,
+    }],
+    'react/destructuring-assignment': ['off'],
+    'jsx-no-constructed-context-values': ['off'],
+    'import/no-extraneous-dependencies': ['off'],
   },
 };
