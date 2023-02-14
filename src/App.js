17b75f5d4733a2bab78be8b00570cc5f76ba1482
liuhuan <aniviah@163.com>
2023-02-14 09:30:18 +0800

    feat: app样式调整

--- src/App.js
+++ src/App.js
@@ -8,44 +8,52 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 
+import './App.css'
+
 import router from './router'
 
 import Home from './views/react-api/Home'
 import Hooks from './views/react-api/hooks'
 import HooksFaq from './views/react-api/hooks-faq'
 import Core from './views/react-api/core'
+import Redux from './views/redux'
 
 export default function App(props) {
   console.log('App---', props)
   return (
     // <RouterProvider router={router}></RouterProvider>
-    <Home />
-    // <Router>
-    //   <div>
-    //     <nav>
-    //       <ul>
-    //         <li>
-    //           <Link to="/">Home</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/hooks">Hooks</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/hooks-faq">Hooks-FAQ</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/core">Core</Link>
-    //         </li>
-    //       </ul>
-    //     </nav>
-
-    //     <Routes>
-    //       <Route path="/hooks" element={<Hooks />}></Route>
-    //       <Route path="/hooks-faq" element={<HooksFaq />}></Route>
-    //       <Route path="/core" element={<Core />}></Route>
-    //       <Route path="/" element={<Home />}></Route>
-    //     </Routes>
-    //   </div>
-    // </Router>
+    // <Home />
+    <Router>
+      <div className="page">
+        <nav className="page-link">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/hooks">Hooks</Link>
+            </li>
+            <li>
+              <Link to="/hooks-faq">Hooks-FAQ</Link>
+            </li>
+            <li>
+              <Link to="/core">Core</Link>
+            </li>
+            <li>
+              <Link to="/redux">Redux</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="page-body" style={{ border: '1px solid green' }}>
+          <Routes>
+            <Route path="/hooks" element={<Hooks />}></Route>
+            <Route path="/hooks-faq" element={<HooksFaq />}></Route>
+            <Route path="/core" element={<Core />}></Route>
+            <Route path="/redux" element={<Redux />}></Route>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
---
+++ src/views/redux/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ReduxPage() {
+  return <div>redux</div>
+}
